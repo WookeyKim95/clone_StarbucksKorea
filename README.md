999e2a3a1ca9e6254953c4e7cce32402a388a2c9
WookeyKim95 <bluebear9508@naver.com>
2022-02-25 19:45:12 +0900

    updating portfolio

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ clone project : making Webpage of starbucks korea
 2022-02-23 : made this repistory
 
 2022-02-24 : added header menu and badges
+[contents](https://wookeykim95.github.io/project/2022/02/24/project-clone_starbucks.html)
+
+2022-02-25 : added toggle and promotion
+[contents](https://wookeykim95.github.io/project/2022/02/25/project-clone_starbucks_2.html)
--- js/main.js
+++ js/main.js
@@ -46,4 +46,48 @@ fade_elements.forEach(function (fade_element, index) {
                                 // 0.7, 1.4, 2.1, 2.7 초 뒤에 각각!
         opacity: 1
     });
-})
+});
+
+// new Swiper('선택자', {옵션})
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical', // 재생 방향
+    autoplay: true, // 자동재생
+    loop: true // 반복재생
+}); // 클래스 생성자, 자스 문법으로 함수를 실행한다.
+
+new Swiper('.promotion .swiper-container', {
+    direction: 'horizontal', // 재생 방향
+    slidesPerView: 3, // 한번에 보일 슬라이드 수
+    spaceBetween: 10, // 슬라이드 사이 여백 (픽셀)
+    centeredSlides: true,
+    loop: true, // 반복재생
+    autoplay: {
+        delay:5000,
+    },
+    pagination: {
+        el:'.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true,  // 클릭이 가능한지?
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next',
+    }
+});
+
+
+const promotion_element = document.querySelector('.promotion');
+const promotion_toggle_button = document.querySelector('.toggle-promotion');
+
+let is_promotion_hidden = false;
+
+let promotion_change = function () {
+    is_promotion_hidden = !is_promotion_hidden;
+    // 값이 반대가 되게 해라, 즉 지금 상태에선 False에서 True로 바꾸어라.
+    if (is_promotion_hidden) {
+        // true : 숨김 처리
+        promotion_element.classList.add('hide');
+    } else {
+        // false : 숨김 해제
+        promotion_element.classList.remove('hide');
+    }
+};
